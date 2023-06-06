672885f93e263a368c19c01b84982d914473f30c
OlenichVladimir <absolut0409@gmail.com>
2023-06-06 10:00:05 +0800

    Main

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,6 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
+    @SuppressWarnings("checkstyle:MethodParamPad")
     public static void main(String[] args) {
         System.out.println("Olenich Vladimir");
         System.out.println("04/09/1989");
