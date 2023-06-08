8b1964291009b95f36459ed22562aa9ede66b3e3
OlenichVladimir <absolut0409@gmail.com>
2023-06-08 16:40:21 +0800

    Main

--- src/main/java/ru/job4j/Calculator2.java
+++ src/main/java/ru/job4j/Calculator2.java
@@ -2,17 +2,17 @@ package ru.job4j;
 
 public class Calculator2 {
 
-    public static void plus (int first, int second) {
+    public static void plus(int first, int second) {
         int result = first + second;
         System.out.println(result);
     }
 
-    public static void minus (int first, int second) {
+    public static void minus(int first, int second) {
         int result = first - second;
         System.out.println(result);
     }
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         plus(1, 2);
         plus(10, 11);
         plus(100, 500);
