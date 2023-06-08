92dbd45f507cc5968a272019bebfcd674186950f
OlenichVladimir <absolut0409@gmail.com>
2023-06-08 18:06:10 +0800

    9.1. String

---
+++ src/main/java/ru/job4j/Greeting.java
@@ -0,0 +1,10 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+    String idea = "I like Java ";
+    idea = idea + "But I am a newbie ";
+    int year = 2023;
+    System.out.println(idea + year);
+    }
+}
