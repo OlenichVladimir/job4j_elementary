5b407bb0c4d9e3af1fa25a85cea984fce2521a11
OlenichVladimir <absolut0409@gmail.com>
2023-06-10 13:53:48 +0800

    9.1. String Исправление

--- src/main/java/ru/job4j/Greeting.java
+++ src/main/java/ru/job4j/Greeting.java
@@ -2,9 +2,9 @@ package ru.job4j.condition;
 
 public class Greeting {
     public static void main(String[] args) {
-    String idea = "I like Java ";
-    idea = idea + "But I am a newbie ";
-    int year = 2023;
-    System.out.println(idea + year);
+        String idea = "I like Java ";
+        idea = idea + "But I am a newbie ";
+        int year = 2023;
+        System.out.println(idea + year);
     }
 }
