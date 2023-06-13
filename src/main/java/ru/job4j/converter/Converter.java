178be5c4ee05f47905f3b23f7eb66db46f6e3abf
OlenichVladimir <absolut0409@gmail.com>
2023-06-13 11:24:18 +0800

    0. Что такое тестирование

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -13,9 +13,14 @@ public class Converter {
     }
 
     public static void main(String[] args) {
-        float euro = rubleToEuro(140);
-        float dollar = rubleToDollar(120);
-        System.out.println("140 rubles are " + euro + " euro.");
-        System.out.println("120 rubles are " + dollar + " dollar.");
+        float in1 = 140;
+        float in2 = 120;
+        float expected = 2;
+        float out1 = rubleToEuro(in1);
+        float out2 = rubleToDollar(in2);
+        boolean passed1 = expected == out1;
+        boolean passed2 = expected == out2;
+        System.out.println("140 rubles are 2 euro. Test result : " + passed1);
+        System.out.println("120 rubles are 2 dollar. Test result : " + passed2);
     }
 }
