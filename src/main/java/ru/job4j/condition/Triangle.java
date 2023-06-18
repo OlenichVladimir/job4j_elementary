4911eec7a3b29c752a99d04bedbcfdb759194626
OlenichVladimir <absolut0409@gmail.com>
2023-06-18 19:00:00 +0800

    4.3.5. Множественное логическое выражение И.

--- src/main/java/ru/job4j/condition/Triangle.java
+++ src/main/java/ru/job4j/condition/Triangle.java
@@ -7,6 +7,6 @@ public class Triangle {
     }
 
     public static void main(String[] args) {
-    System.out.println(exist(2.0, 2.0, 2.0));
+        System.out.println(exist(2.0, 2.0, 2.0));
     }
 }
